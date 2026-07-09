852ccad8832e134ab27f83981e67461cf44f6c6c
markhristov <marki.v.hristov@gmail.com>
2026-07-09 16:51:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a30_days_of_movement/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.a30_days_of_movement
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.a30_days_of_movement.ui.theme.A30DaysOfMovementTheme
+import com.example.a30_days_of_movement.ui.theme._30_days_of_movementTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            A30DaysOfMovementTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    A30DaysOfMovementTheme {
+        Greeting("Android")
+    }
+}
