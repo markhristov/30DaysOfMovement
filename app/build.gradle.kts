29b04acf716eb1f460c16f04edb64f347a4a73df
markhristov <marki.v.hristov@gmail.com>
2026-07-10 08:08:45 +0300

    App done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.a30_days_of_movement"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.a30_days_of_movement"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
@@ -43,6 +39,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.text.google.fonts)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
